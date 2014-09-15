1f188d9095b492286075fdaffcd7208673f78a74
Shane Sniteman <ssniteman@gmail.com>
2014-09-15 09:33:49 -0400

    StoryBoard Connected

---
+++ Loweet/Loweet/TweetLocationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetLocationTableViewCell.swift
+//  Loweet
+//
+//  Created by Shane Sniteman on 9/15/14.
+//  Copyright (c) 2014 Shane Sniteman. All rights reserved.
+//
+
+import UIKit
+
+class TweetLocationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
